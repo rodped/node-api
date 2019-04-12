10ac7181d6e2ae76d09392ec9f8258141687c00e
Pedro Asseiro Rodrigues <p.rodrigues@hotmail.com>
2019-04-12 16:44:38 +0100

    Instalado MongoDB e feita a conexão

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const mongoose =require('mongoose');
 
+// Iniciando a App
 const app = express();
 
+// Iniciando a DB
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi',
+     {useNewUrlParser: true}
+);
+
+// Primeira rota
 app.get('/', (req, res) => {
     res.send('Hello!!!');
 });
