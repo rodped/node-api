faab8cf86396a86f5d95d5dd305e438d61b874b1
Pedro Asseiro Rodrigues <p.rodrigues@hotmail.com>
2019-04-12 17:02:59 +0100

    Primeiro registo na base de dados

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
-const express = require('express');
-const mongoose =require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
 
 // Iniciando a App
 const app = express();
 
 // Iniciando a DB
 mongoose.connect(
-    'mongodb://localhost:27017/nodeapi',
-     {useNewUrlParser: true}
+    "mongodb://localhost:27017/nodeapi",
+    { useNewUrlParser: true }
 );
+requireDir("./src/models");
+
+const Product=mongoose.model("Product");
 
 // Primeira rota
-app.get('/', (req, res) => {
-    res.send('Hello!!!');
+app.get("/", (req, res) => {
+    Product.create({
+        title: "React Native",
+        description: "Build native apps with React",
+        url: "http://github.com/facebook/react-native"
+    });
+
+
+    res.send("Hello!!!");
 });
 
 app.listen(3001);
